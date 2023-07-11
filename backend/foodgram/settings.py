4c7554cb56790e64ab5a2e5f2d4ca9efd9aca18c
Pavel Melnikov <room.apps@mail.ru>
2023-07-11 17:36:33 +0300

    Update settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", 'django-insecure-123')
 
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG', True))
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
