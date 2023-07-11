cba54c438c7a9fa540d1965f5eac8fc87684c2fd
Pavel Melnikov <room.apps@mail.ru>
2023-07-11 17:50:19 +0300

    Edited serializers & admin.

--- backend/api/v1/users/serializers.py
+++ backend/api/v1/users/serializers.py
@@ -2,9 +2,7 @@ from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework.fields import SerializerMethodField
 from rest_framework.exceptions import ValidationError
-from rest_framework import status
-
-from rest_framework import serializers
+from rest_framework import serializers, status
 
 from users.models import User
 from ..serializers import RecipeShortSerializer
--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import (
-    Favorites, Ingredients, Recipe, RecipeIngredient, ShoppingCart, Tags,
-)
-
-from django.contrib import admin
+    Favorites, Ingredients, Recipe,
+    RecipeIngredient, ShoppingCart, Tags, )
 
 
 @admin.register(Ingredients)
