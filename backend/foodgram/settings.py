1572b203a42b700f6785fa5a19e1f3b0528360f3
Pavel Melnikov <room.apps@mail.ru>
2023-07-14 12:14:39 +0300

    Try to deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -99,7 +99,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
