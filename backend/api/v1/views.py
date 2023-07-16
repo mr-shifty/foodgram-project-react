3b22f6b1d81671f3f4c2e39cf625c308c2a200f7
Pavel Melnikov <room.apps@mail.ru>
2023-07-16 21:32:36 +0300

    Fix bugs v1

--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -2,7 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import (
-    IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
+    AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly,
 )
 from rest_framework.response import Response
 
